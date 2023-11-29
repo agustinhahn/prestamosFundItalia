baca8f9fd383236b943d00a9479f2c6f88c5159b
Agustin Hahn <agustin.f.hahn@gmail.com>
2023-11-29 07:19:39 -0300

    cambios logica wp

--- src/pages/Agradecimiento.jsx
+++ src/pages/Agradecimiento.jsx
@@ -1,42 +1,39 @@
-import { useContext, useState, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ContextCart } from '../context/CartContext';
 
-
 const Agradecimiento = () => {
+    const { cbu, setCbu } = useContext(ContextCart);
+    const { nombreCliente, setnombreCliente } = useContext(ContextCart);
+    const { dniCliente, setDniCliente } = useContext(ContextCart);
+    const { cuotaElegida, setCuotaElegida } = useContext(ContextCart);
+    const { importElegido, setImportElegido } = useContext(ContextCart);
 
-
-    const {cbu,setCbu} = useContext(ContextCart)
-    const {nombreCliente,setnombreCliente} = useContext(ContextCart)
-    const {dniCliente,setDniCliente} = useContext(ContextCart)
-    const {cuotaElegida, setCuotaElegida} = useContext(ContextCart)
-    const {importElegido, setImportElegido} = useContext(ContextCart)
-
-
-    const valorxCuota =(importElegido / cuotaElegida).toFixed(2)
-    
+    const valorxCuota = (importElegido / cuotaElegida).toFixed(2);
 
     const enviarWhatsapp = () => {
-        const numeroCelular = '3414684871'; 
-        const mensajesCombinados = encodeURIComponent(`Hola Mario, te contacto por un prestamo.\nMi nombre es: ${nombreCliente}\nMi documento: ${dniCliente}\nMi CBU: ${cbu}\nQuiero recibir: $${importElegido}\nCantidad de cuotas: ${cuotaElegida} \nEstare pagando por cuota: $${valorxCuota}`);
+        const numeroCelular = '3414684871';
+        const mensajesCombinados = encodeURIComponent(
+            `Hola Mario, te contacto por un prestamo.\nMi nombre es: ${nombreCliente}\nMi documento: ${dniCliente}\nMi CBU: ${cbu}\nQuiero recibir: $${importElegido}\nCantidad de cuotas: ${cuotaElegida} \nEstare pagando por cuota: $${valorxCuota}`
+        );
         const enlaceWhatsapp = `https://wa.me/${numeroCelular}?text=${mensajesCombinados}`;
-        window.open(enlaceWhatsapp, '_blank');
-        window.location.href= "/"
+        window.location.href = enlaceWhatsapp;
     };
 
     useEffect(() => {
-        console.log("hola de useefect")
+        console.log("hola de useEffect");
         const delay = 3000;
         const timeoutId = setTimeout(enviarWhatsapp, delay);
-    },[])
-
+    }, []);
 
     return (
-        <div className='containerApp'>
-            <h1 className='tituloh1'>gracias por tu solicitud</h1>
-            <p className='info'>tu prestamos esta siendo evaluado y en breve tendras noticias nuestras</p>
-            <button>Volver al inicio</button> 
+        <div className="containerApp">
+            <h1 className="tituloh1">Gracias por tu solicitud</h1>
+            <p className="info">Tu préstamo está siendo evaluado y en breve tendrás noticias nuestras</p>
+            <a href="#" onClick={enviarWhatsapp}>
+                Abrir WhatsApp
+            </a>
         </div>
-    )
-}
+    );
+};
 
-export default Agradecimiento
+export default Agradecimiento;
